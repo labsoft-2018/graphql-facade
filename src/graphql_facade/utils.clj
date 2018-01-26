9945a561d6fa03846ce6f9ac540445da98eb9e97
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-01-26 15:06:11 -0200

    Utils

--- src/graphql_facade/utils.clj
+++ src/graphql_facade/utils.clj
@@ -1,1 +1,10 @@
 (ns graphql-facade.utils)
+
+(defn unnamespaced [str-or-kw]
+  (-> str-or-kw keyword name keyword))
+
+(defn unnamespaced-map [map]
+  (clojure.walk/postwalk #(if (keyword? %) (keyword (name %)) %) map))
+
+(defn namespaced [ns str-or-kw]
+  (keyword ns (name str-or-kw)))
